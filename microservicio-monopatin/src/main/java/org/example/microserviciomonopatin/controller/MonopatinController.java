e26a0a993557304ff74e0f24728cce1de254fcfc
Bruno De la Penna <bdelapenna@gmail.com>
2025-11-04 17:54:29 -0300

    Realizando ABM de Monopatin

--- microservicio-monopatin/src/main/java/org/example/microserviciomonopatin/controller/MonopatinController.java
+++ microservicio-monopatin/src/main/java/org/example/microserviciomonopatin/controller/MonopatinController.java
@@ -1,16 +1,59 @@
 package org.example.microserviciomonopatin.controller;
 
 
+import org.example.microserviciomonopatin.entity.Monopatin;
 import org.example.microserviciomonopatin.repository.MonopatinRepository;
+import org.example.microserviciomonopatin.service.MonopatinService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/monopatin")
 public class MonopatinController {
 
     @Autowired
-    private MonopatinRepository monopatinRepository;
+    private MonopatinService monopatinService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Monopatin>> getAll() {
+        List<Monopatin> monopatines = monopatinService.getAll();
+        if (monopatines.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return  ResponseEntity.ok(monopatines);
+    }
+
+
+    @PostMapping("")
+    public ResponseEntity<Monopatin> save(@RequestBody Monopatin monopatin) {
+        Monopatin monopatinNew = monopatinService.save(monopatin);
+        return  ResponseEntity.ok(monopatinNew);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Monopatin> delete(@PathVariable("id") Long id) {
+        Monopatin monopatin = monopatinService.findById(id);
+        monopatinService.deleteById(id);
+        return ResponseEntity.ok(monopatin);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Monopatin> update(@PathVariable("id") Long id, @RequestBody Monopatin monopatin) {
+        Monopatin monopatinNew = monopatinService.findById(id);
+        if (monopatinNew == null) {
+            return ResponseEntity.notFound().build();
+        }
+        monopatinNew.setEstado(monopatin.getEstado());
+        monopatinNew.setLatitud(monopatin.getLatitud());
+        monopatinNew.setLongitud(monopatin.getLongitud());
+        monopatinNew.setKmsAcumulados(monopatin.getKmsAcumulados());
+        monopatinNew.setId_parada(monopatin.getId_parada());
+        monopatinNew.setId_viaje(monopatin.getId_viaje());
+        return   ResponseEntity.ok(monopatinService.save(monopatinNew));
+    }
 }
--- microservicio-monopatin/src/main/java/org/example/microserviciomonopatin/service/MonopatinService.java
+++ microservicio-monopatin/src/main/java/org/example/microserviciomonopatin/service/MonopatinService.java
@@ -1,12 +1,35 @@
 package org.example.microserviciomonopatin.service;
 
 
+import org.example.microserviciomonopatin.entity.Monopatin;
 import org.example.microserviciomonopatin.repository.MonopatinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MonopatinService {
     @Autowired
     private MonopatinRepository monopatinRepository;
+
+    public Monopatin save(Monopatin monopatin) {
+        return monopatinRepository.save(monopatin);
+    }
+
+    public List<Monopatin> getAll() {
+        return  monopatinRepository.findAll();
+    }
+
+    public Monopatin findById(Long id) {
+        return monopatinRepository.findById(id).orElse(null);
+    }
+
+    public void deleteById(Long id) {
+        monopatinRepository.deleteById(id);
+    }
+
+    public Monopatin update(Monopatin monopatin) {
+        return  monopatinRepository.save(monopatin);
+    }
 }
