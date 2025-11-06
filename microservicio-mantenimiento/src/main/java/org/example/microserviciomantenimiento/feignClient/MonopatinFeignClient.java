d7ec95a7541507f6de86576d90e77ac1902ce5b7
Maximo Bayones <bayonesmaximo20@gmail.com>
2025-11-06 17:24:05 -0300

    borre config

--- microservicio-mantenimiento/src/main/java/org/example/microserviciomantenimiento/feignClient/MonopatinFeignClient.java
+++ microservicio-mantenimiento/src/main/java/org/example/microserviciomantenimiento/feignClient/MonopatinFeignClient.java
@@ -3,6 +3,6 @@ package org.example.microserviciomantenimiento.feignClient;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name="microservicio-monopatin", url="http://localhost:800x/monopatines")
+@FeignClient(name="microservicio-monopatin", url="http://localhost:8007/monopatines")
 public interface MonopatinFeignClient {
 }
