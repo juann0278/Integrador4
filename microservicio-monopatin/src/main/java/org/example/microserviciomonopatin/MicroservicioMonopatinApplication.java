152771933c4af7451e75bd4ad21d1e02c04dd00e
Maximo Bayones <bayonesmaximo20@gmail.com>
2025-11-07 18:12:26 -0300

    comentario para feignclient

--- microservicio-monopatin/src/main/java/org/example/microserviciomonopatin/MicroservicioMonopatinApplication.java
+++ microservicio-monopatin/src/main/java/org/example/microserviciomonopatin/MicroservicioMonopatinApplication.java
@@ -2,8 +2,11 @@ package org.example.microserviciomonopatin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MicroservicioMonopatinApplication {
 
     public static void main(String[] args) {
